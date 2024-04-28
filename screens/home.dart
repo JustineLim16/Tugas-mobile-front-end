8d1c3bcd2903934178891c4f79ab7f56ff9c9ee8
Kenzyl <138746818+Kenzyl@users.noreply.github.com>
2024-04-28 21:14:12 +0700

    Update home.dart

--- screens/home.dart
+++ screens/home.dart
@@ -24,69 +24,6 @@ class _HomeScreenState extends State<Home> {
 
   String? _radioValue = "";
 
-  List<Map<String, dynamic>> all = [
-  {
-    "category": "income",
-    "description": "Transfer",
-    "date": "2024-04-02",
-    "price": 500000
-  },
-  {
-    "category": "income",
-    "description": "Saving",
-    "date": "2024-04-02",
-    "price": 2000000
-  },
-  {
-    "category": "expense",
-    "description": "Clothes",
-    "date": "2024-04-02",
-    "price": 1000000
-  },
-  {
-    "category": "expense",
-    "description": "Food",
-    "date": "2024-04-02",
-    "price": 200000
-  },
-  {
-    "category": "expense",
-    "description": "Transfer",
-    "date": "2024-04-02",
-    "price": 1000000
-  },
-  {
-    "category": "income",
-    "description": "Transfer",
-    "date": "2024-04-02",
-    "price": 200000
-  },
-  {
-    "category": "expense",
-    "description": "Food",
-    "date": "2024-04-01",
-    "price": 150000
-  },
-  {
-    "category": "expense",
-    "description": "Subscription",
-    "date": "2024-04-01",
-    "price": 500000
-  },
-  {
-    "category": "expense",
-    "description": "Subscription",
-    "date": "2024-04-01",
-    "price": 1000000
-  },
-  {
-    "category": "income",
-    "description": "Salary",
-    "date": "2024-04-01",
-    "price": 4500000
-  },
-];
-
   @override
   void initState() {
     super.initState();
