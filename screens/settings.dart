6c9d4b06df80ed8c376049fbfc8da0600c3b2c8b
Kenzyl <138746818+Kenzyl@users.noreply.github.com>
2024-05-07 00:28:58 +0700

    Update settings.dart

--- screens/settings.dart
+++ screens/settings.dart
@@ -1,84 +1,188 @@
+import 'package:chocobi/screens/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:chocobi/screens/home.dart';
+import 'package:chocobi/screens/testCard.dart';
+import 'package:chocobi/screens/transaction.dart';
+import 'package:provider/provider.dart';
 
-class SettingsPage extends StatefulWidget {
+class Profile extends StatefulWidget {
   @override
-  _SettingsPageState createState() => _SettingsPageState();
+  State<Profile> createState() => _ProfileState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
-  bool _isDarkMode = false;
-  bool _pushNotifications = false;
-  bool _soundNotifications = false;
-  String _selectedLanguage = 'English';
+class _ProfileState extends State<Profile> {
+  List light = [Colors.white, Colors.black];
+  List dark = [Colors.black, Colors.white];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 17, 80, 156),
-          title: Text(
-            'Settings',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            color: Color.fromARGB(255, 17, 80, 156),
+            height: MediaQuery.of(context).size.height/4,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 51,
+                    backgroundColor: Colors.black,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage('lib/assets/profile.jpg'),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text("Shinchan", style: TextStyle(fontSize: 22, color: Colors.white)),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.edit_rounded, color: Colors.white, size: 18,))
+                        ],
+                      ),
+                      Text("shinchan@gmail.com", style: TextStyle(fontSize: 19, color: Colors.white, fontStyle: FontStyle.italic)),
+                      Text("08116075384", style: TextStyle(fontSize: 19, color: Colors.white, fontStyle: FontStyle.italic)),
+                    ],
+                  )
+                ],
+              ),
+            )
           ),
-        ),
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text('Dark Mode'),
-              trailing: Switch(
-                value: _isDarkMode,
-                onChanged: (value) {
-                  setState(() {
-                    _isDarkMode = value;
-                  });
-                },
-              ),
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text('Account'),
+            onTap: () {
+              // Aksi untuk 'Account'
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.upload),
+            title: Text('Export Data'),
+            onTap: () {
+              // Aksi untuk 'Export Data'
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              // Aksi untuk 'Logout'
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 90,
+        color: Provider.of<SettingsModel>(context).isDarkMode ? dark[0] : light[0],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(icon: Icon(Icons.home, size: 30), onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Home()),
+                    ModalRoute.withName("/Profile"));
+                }),
+                Text('Home',style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold)), 
+              ],
             ),
-            ListTile(
-              title: Text('Push Notifications'),
-              trailing: Switch(
-                value: _pushNotifications,
-                onChanged: (value) {
-                  setState(() {
-                    _pushNotifications = value;
-                  });
-                },
-              ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(icon: Icon(Icons.swap_horiz, size: 30), onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => Transaction(income_selected: true, expense_selected: true,)),
+                    ModalRoute.withName("/Profile"));
+                } ),
+                Text('Transaction',style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold)), 
+              ],
             ),
-            ListTile(
-              title: Text('Sound Notifications'),
-              trailing: Switch(
-                value: _soundNotifications,
-                onChanged: (value) {
-                  setState(() {
-                    _soundNotifications = value;
-                  });
-                },
-              ),
-            ),
-            ListTile(
-              title: Text('Language'),
-              trailing: DropdownButton<String>(
-                value: _selectedLanguage,
-                onChanged: (newValue) {
-                  setState(() {
-                    _selectedLanguage = newValue!;
-                  });
-                },
-                items: <String>['English', 'Indonesian', 'French', 'German']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
+            FloatingActionButton(
+              backgroundColor: Color.fromARGB(255, 21, 96, 189),
+              child: Icon(Icons.add, color: Provider.of<SettingsModel>(context).isDarkMode ? dark[0] : light[0]),
+              onPressed: () {
+                showDialog(context: context, builder: (context){
+                  return SizedBox(
+                    height: 400,
+                    child: AlertDialog(
+                      title: Text("Add Transaction ?"),
+                      actions: [
+                        TextButton(onPressed: (){
+                          showDialog(context: context, builder: (context){
+                            return SizedBox(
+                              height: 400,
+                              child: AlertDialog(
+                                title: Text("Insert Transaction"),
+                                actions: [
+                                  TextButton(onPressed: (){}, child: Text("ADD", style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),)),
+                                  TextButton(onPressed: (){}, child: Text("CANCEL", style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),)),
+                                ],
+                                content: SizedBox(
+                                  height: 400,
+                                  width: 350,
+                                  child: Column(children: [
+                                    TextField(
+                                      decoration: InputDecoration(
+                                        hintText: "Masukkan Judul",
+                                        enabledBorder: OutlineInputBorder(),
+                                        focusedBorder: OutlineInputBorder(),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                        hintText: "Masukkan Jumlah",
+                                        enabledBorder: OutlineInputBorder(),
+                                        focusedBorder: OutlineInputBorder(),),
+                                    )],),
+                                ),
+                              ),
+                            );
+                          });
+                        }, child: Text("ADD",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),)),
+                        TextButton(onPressed: (){}, child: Text("CANCEL",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),))
+                      ],
+                    ),
                   );
-                }).toList(),
-              ),
+                });
+              }, 
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(icon: Icon(Icons.account_balance_wallet, size: 30), onPressed: () {
+                  showDialog(context: context, builder: (context){return const TestCard();});
+                }),
+                Text('Budget',style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold)), 
+              ],
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(icon: Icon(Icons.person, size: 30, color: Color.fromARGB(255, 17, 80, 156)), onPressed: () {}),
+                Text('Profile',style: TextStyle(fontSize: 11, color: Color.fromARGB(255, 17, 80, 156), fontWeight: FontWeight.bold)), 
+              ],
             ),
           ],
         ),
