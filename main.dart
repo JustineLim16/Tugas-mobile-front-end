6a553078f30c580a25b812497f1abebded66e313
Kenzyl <138746818+Kenzyl@users.noreply.github.com>
2024-05-07 18:43:42 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,20 +1,29 @@
-import 'package:flutter/material.dart';
+import 'package:chocobi/screens/home.dart';
+import 'package:chocobi/screens/profile.dart';
+import 'package:chocobi/screens/settings.dart';
 import 'package:chocobi/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => SettingsModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ super.key });
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Money Tracker',
-      theme: ThemeData(fontFamily: 'Open Sans'),
-      home: const SplashScreen(),
+      title: 'Chocobi',
+      theme: Provider.of<SettingsModel>(context).isDarkMode  ? ThemeData.dark()  : ThemeData.light(),
+      home:  Home(),
     );
   }
 }
