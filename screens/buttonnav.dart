0b9ddd3f843742b00fc86fc9991892fa56f65312
Darwin <59282566+medanjaya@users.noreply.github.com>
2024-07-04 23:55:23 +0700

    menghapus TestCard yang sudah tidak digunakan

--- screens/buttonnav.dart
+++ screens/buttonnav.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
+import 'package:chocobi/screens/budget.dart';
 import 'package:chocobi/screens/settings.dart';
+import 'package:flutter/material.dart';
 import 'package:chocobi/screens/home.dart';
 import 'package:chocobi/screens/profile.dart';
 import 'package:chocobi/screens/transaction.dart';
+import 'package:provider/provider.dart';
 
 class CustomBottomAppBar extends StatefulWidget {
-  const CustomBottomAppBar({super.key});
-
   @override
   State<CustomBottomAppBar> createState() => _CustomBottomAppBarState();
 }
@@ -28,8 +26,8 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
           _buildBottomAppBarItem(Icons.home, 'Home', () {
             Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(builder: (context) => const HomePage()),
-              ModalRoute.withName("/HomePage"),
+              MaterialPageRoute(builder: (context) => Home()),
+              ModalRoute.withName("/Home"),
             );
           }),
           _buildBottomAppBarItem(Icons.swap_horiz, 'Transaction', () {
@@ -46,15 +44,13 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
               _showAddTransactionDialog(context); // Panggil fungsi yang menampilkan dialog
             },
           ),
-          _buildBottomAppBarItem(
-            Icons.account_balance_wallet, 'Budget', () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(///
-                  content: Text('Function to be implemented')
-                ),
-              );
-            }
-          ),
+          _buildBottomAppBarItem(Icons.account_balance_wallet, 'Budget', () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => Budget()),
+              ModalRoute.withName("/Home"),
+            );
+          }),
           _buildBottomAppBarItem(Icons.person, 'Profile', () {
             Navigator.pushAndRemoveUntil(
               context,
@@ -96,7 +92,7 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
                         children: [
                           Expanded(
                             child: ListTile(
-                              title: const Text("Income"),
+                              title: Text("Income"),
                               leading: Radio<String>(
                                 value: "income",
                                 groupValue: _radioValue,
@@ -168,7 +164,7 @@ class _CustomBottomAppBarState extends State<CustomBottomAppBar> {
                 if (_formKey.currentState!.validate()) {
                   // If the form is valid, display a snackbar and add the transaction
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
+                    SnackBar(content: Text('Processing Data')),
                   );
                   Navigator.pop(context); // Close the dialog
                 }
