7c4fe52683927a142dee1f618b91ce33e9d572fb
Darwin <59282566+medanjaya@users.noreply.github.com>
2024-07-04 23:54:13 +0700

    penambahan key "phone" & "profile" pada users.dart

--- data/users.dart
+++ data/users.dart
@@ -3,18 +3,21 @@ List<Map<String, dynamic>> users = [
     "username": "admin-chocobi",
     "email": "admin@gmail.com",
     "password": "1234567.",
+    "phone" : "0811634715",
     "profile" : ""
   },
   {
     "username": "Shinchan",
     "email": "shinchan@gmail.com",
     "password": "keluarganohara",
+    "phone" : "0895612615150",
     "profile" : "https://i.pinimg.com/736x/88/08/6b/88086b5e12f8a2146ea48e37070819fa.jpg"
   },
   {
     "username": "medanjaya",
     "email": "peoplecallmed@gmail.com",
     "password": "test1234",
+    "phone" : "089613713685",
     "profile" : "https://shop.theheartworm.com/cdn/shop/products/wmtgg2gd3himg8721heb_1024x.jpg"
   }
 ];
