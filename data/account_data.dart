53fcea6a95c20399d1fe61cfe721e6e4e5776c12
Kenzyl <138746818+Kenzyl@users.noreply.github.com>
2024-07-07 00:52:11 +0700

    Menambahkan account_data untuk bagian profile user

---
+++ data/account_data.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfileNotifier extends ChangeNotifier {
+  Map<String, dynamic> _accountInfo = {
+    "profilePic": "lib/assets/profile.jpg",
+    "name": "Shinchan",
+    "email": "shinchan@gmail.com",
+    "phone": "08116075384",
+    "picStatus": "asset"
+  };
+
+  Map<String, dynamic> get accountInfo => _accountInfo;
+
+  void updateProfilePic(String image, String stats) {
+    _accountInfo["profilePic"] = image;
+    _accountInfo["picStatus"] = stats;
+    notifyListeners();
+  }
+
+  void updateProfileData(String info, String data) {
+    _accountInfo[info] = data;
+    notifyListeners();
+  }
+}
